12a0b957b4187fdf72da5871897c7fa2de89ea8c
tanaka shota <tanaka@n-di.jp>
2023-12-06 12:05:59 +0900

    first commit

---
+++ Event.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\NewArrival42;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class Event implements EventSubscriberInterface
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [];
+    }
+}
---
+++ TwigBlock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\NewArrival42;
+
+use Eccube\Common\EccubeTwigBlock;
+
+class TwigBlock implements EccubeTwigBlock
+{
+    /**
+     * @return array
+     */
+    public static function getTwigBlock()
+    {
+        return [];
+    }
+}
